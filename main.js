bb392c72928734e0734d8a6ff7d544dcfe6f7586
Jinny <dms873@naver.com>
2021-05-28 10:04:15 +0900

    Fix : Active navbar_menu with Scroll 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Make navbar trasparent when it is on the top
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -21,6 +21,7 @@ navbarMenu.addEventListener("click", (event) => {
   }
   navbarMenu.classList.remove("open");
   scrollIntoView(link);
+  selectedNavItem(target);
 });
 
 // Navbar toggle button for small screen
@@ -37,15 +38,15 @@ homeContactBtn.addEventListener("click", () => {
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
-const homeHieght = home.getBoundingClientRect().height;
+const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-  home.style.opacity = 1 - window.scrollY / homeHieght;
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Show "arrow up" button when scrolling down
+// Show "arrow up button when scrolling down"
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
-  if (window.scrollY > homeHieght / 2) {
+  if (window.scrollY > homeHeight / 2) {
     arrowUp.classList.add("visible");
   } else {
     arrowUp.classList.remove("visible");
@@ -66,7 +67,6 @@ workBtnContainer.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
-
   // Remove selection from the previous item and select the new one
   const active = document.querySelector(".category__btn.selected");
   active.classList.remove("selected");
@@ -77,13 +77,13 @@ workBtnContainer.addEventListener("click", (e) => {
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
         project.classList.add("invisible");
       }
     });
+
     projectContainer.classList.remove("anim-out");
   }, 300);
 });
@@ -93,9 +93,9 @@ function scrollIntoView(selector) {
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
 
-// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
-// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
-// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
 
 const sectionIds = [
   "#home",
@@ -109,8 +109,14 @@ const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[data-link="${id}"]`)
 );
-console.log(sections);
-console.log(navItems);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
+}
 
 const observerOptions = {
   root: null,
@@ -120,8 +126,27 @@ const observerOptions = {
 
 const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
-    console.log(entry.target);
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
   });
 };
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach((section) => observer.observe(section));
+
+window.addEventListener("wheel", () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.ceil(window.scrollY + window.innerHeight) >= document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
