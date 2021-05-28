b339e91aec0f5f1e9d31f9def27157b10805f1a4
Jinny <dms873@naver.com>
2021-05-28 10:08:34 +0900

    Fix : Active navbar_menu with Scroll 3

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ navbarMenu.addEventListener("click", (event) => {
   }
   navbarMenu.classList.remove("open");
   scrollIntoView(link);
-  selectedNavItem(target);
 });
 
 // Navbar toggle button for small screen
@@ -88,11 +87,6 @@ workBtnContainer.addEventListener("click", (e) => {
   }, 300);
 });
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
 // 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다.
 // 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
 // 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
@@ -116,6 +110,12 @@ function selectNavItem(selected) {
   selectedNavItem.classList.remove("active");
   selectedNavItem = selected;
   selectedNavItem.classList.add("active");
+}
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 const observerOptions = {
